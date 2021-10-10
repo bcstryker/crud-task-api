42bd94cc668e80baa8381fcdd0910dfe40b38df6
brandon <bcstryker@gmail.com>
2021-10-09 20:25:13 -0400

    Added local error message to mongoose.js

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/taskmanager')
     .then(() => console.log('DB connected successfully!'))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log('Error occurred while connecting to DB', error));
 
 module.exports = mongoose;
